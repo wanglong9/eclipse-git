ce11fb22a8fbb8474352f40b17d713cf60c860c8
wanglong <416918328@qq.com>
2015-04-21 18:20:59 +0800

    branch

--- src/main/java/com/ai/git_test/FirstGitTest.java
+++ src/main/java/com/ai/git_test/FirstGitTest.java
@@ -2,11 +2,6 @@ package com.ai.git_test;
 
 public class FirstGitTest {
 	public static void main(String[] args) {
-		System.out.println("Hello Git");
-		System.out.println("Hello Three");
-		System.out.println("Hello Git too");
-		System.out.println("Hello Git four");
-		System.out.println("Hello Git five");
-		System.out.println("Hello Git six");
+		System.out.println("Hello Git branch");
 	}
 }
